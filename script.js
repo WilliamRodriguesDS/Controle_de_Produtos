4f48f29e4767fc4d83de12275567a3ba572fa01e
William Rodrigues <william.oliveira@infobrasil.local>
2024-10-08 16:02:32 -0300

    sla

--- script.js
+++ script.js
@@ -36,19 +36,28 @@ function criarProduto() {
     let nome = prompt("Escreva o nome do novo produto");
     let preco = parseFloat(prompt("Digite um preço"));
 
-    if (nome && preco) {
-        let novoProduto = { "id": produtos.length + 1, "nome": nome, "preco": preco };
+    if(nome && preco) {
+        let novoProduto = { 
+            "id": produtos.length + 1, 
+            "nome": nome, 
+            "preco": preco 
+        };
         produtos.push(novoProduto);
         alert("O produto foi adicionado com sucesso");
     }
-    else {
-        alert("O nome e o preço são obrigatórios");
-        return criarProduto();
+        else{
+            alert("O nome e o preço são obrigatórios");
+            return criarProduto();
+        }
     }
-}
 
 function lerProduto(){
     let msg = "";
+
+    if(produtos.length === 0){
+        return alert("Não há produtos registrados no momento");
+    }
+
     for(let i = 0; i < produtos.length; i++){
         msg += "ID: " + produtos[i].id + "\n";
         msg += "Nome: " + produtos[i].nome + "\n";
@@ -59,13 +68,40 @@ function lerProduto(){
 }
 
 function deletarProduto(){
-    let delet = parseInt(prompt("Digite o número do id para deletar o produto"));
-    if(delet){
-        for(let i = 0; i < produtos.length; i++){
-            if(produtos.id === delet){
+    let deletarIDs = prompt("Digite o número do id para deletar o produto");
+    let listaIDs = deletarIDs.split(",").map(Number);
+
+    if(produtos.length === 0){
+        return alert("Não há produtos registrados que possam ser deletados")
+    }
+
+    if(listaIDs.length > 0){
+        for(let i = produtos.length - 1; i >= 0; i--){
+            if(listaIDs.includes(produtos[i].id)){
                 produtos.splice(i, 1);
-                alert("Produto deletado com sucesso");
             }
         }
+        alert("Produto(s) deletado(s) com sucesso");
     }
+}
+function atualizarProduto(){
+    let solicitarID = parseInt(prompt("Digite o número do ID que deseja atualizar"));
+
+    if(solicitarID)
+        for(let i = 0; i < produtos.length; i++){
+            
+            if(produtos[i].id === solicitarID);
+            let atualizar = parseInt(prompt("O que deseja atualizar?\n 1 - nome\n 2 - preço"));
+            
+            switch(atualizar){
+                case 1:
+                    let nome = prompt("O que deseja mudar no nome?\n Nome antigo: " + produtos[i].nome);
+                    produtos[i].nome = nome;
+                    alert("Nome mudado com sucesso");
+                case 2:
+                    let preco = parseFloat(prompt("O que deseja mudar no preço\n Preço antigo: " + produtos[i].preco));
+                    produtos[i].nome = preco;
+                    alert("Preço mudado com sucesso");
+            }
+        }
 }
